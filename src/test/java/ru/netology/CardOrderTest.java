6abdb45b07c9687f71d6f78a86e01a7ec9b111ed
Alexandr-1221 <ags.sad.1221@gmail.com>
2021-04-04 01:04:24 +0300

    create repository

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+
+    @Test
+    void shouldSubmittingAValidForm(){
+        open("http://localhost:9999");
+        $("[type=\"text\"]").setValue("Иван Иванов");
+        $("[type=\"tel\"]").setValue("+71111111111");
+        $("[class=\"checkbox__box\"]").click();
+        $("button").click();
+        $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
