41b6268aedffc338e85ad1bcbc69de058368570e
Alexandr-1221 <ags.sad.1221@gmail.com>
2021-04-04 01:31:56 +0300

    Validation check

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -19,4 +19,41 @@ public class CardOrderTest {
         $("button").click();
         $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void shouldSubmittingInvalidFieldAName(){
+        open("http://localhost:9999");
+        $("[type=\"text\"]").setValue("Ivan Petrov");
+        $("[type=\"tel\"]").setValue("+71111111111");
+        $("[class=\"checkbox__box\"]").click();
+        $("button").click();
+        $("[data-test-id=\"name\"] [class=\"input__sub\"]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSubmittingInvalidFieldAPhone(){
+        open("http://localhost:9999");
+        $("[type=\"text\"]").setValue("Иван Иванов");
+        $("[type=\"tel\"]").setValue("7114611111111");
+        $("[class=\"checkbox__box\"]").click();
+        $("button").click();
+        $("[data-test-id=\"phone\"] [class=\"input__sub\"]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldSubmittingEmptyFields(){
+        open("http://localhost:9999");
+        $("[class=\"checkbox__box\"]").click();
+        $("button").click();
+        $("[data-test-id=\"name\"] [class=\"input__sub\"]").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSubmittingEmptyFieldAPhone(){
+        open("http://localhost:9999");
+        $("[type=\"text\"]").setValue("Иван Иванов");
+        $("[class=\"checkbox__box\"]").click();
+        $("button").click();
+        $("[data-test-id=\"phone\"] [class=\"input__sub\"]").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
 }
